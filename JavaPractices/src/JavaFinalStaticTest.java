be5dda20c1b5ba7aa73c41e9464f2aa17cfa0758
rames <rameshmoorjani@gmail.com>
2017-12-10 13:59:10 -0600

    my second git hub files

---
+++ JavaPractices/src/JavaFinalStaticTest.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class JavaFinalStaticTest {
+	
+	private static final List foo=new ArrayList();
+	
+	private static final List foo1=new ArrayList();
+	
+	public JavaFinalStaticTest()
+	{
+	//	foo= new ArrayList();
+		
+	//	foo.add("Geeta");
+	}
+	
+	public static void main(String[] args) {
+		
+		JavaFinalStaticTest finalStaticTest = new JavaFinalStaticTest();
+		
+		finalStaticTest.foo.add("Ramesh");
+		
+		foo=foo1;
+		
+		System.out.println("list is "+finalStaticTest.foo);
+		
+		finalStaticTest.foo.add("Geeta");
+		
+		
+		
+		System.out.println("list is "+finalStaticTest.foo);
+	}
+	
+
+}
