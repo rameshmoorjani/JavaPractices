ae19daf9a12621bad8d2bd9711d91d63bb12b5ba
ramesh ramesh <rameshmoorjani@gmail.com>
2020-09-03 21:30:35 -0700

    'added30'

--- JavaPractices/src/CollectionStream.java
+++ JavaPractices/src/CollectionStream.java
@@ -15,6 +15,7 @@ public class CollectionStream {
 		al.add(25);
 		al.add(20);
 		a1.add(25);
+		a1.add(30);
 		  
 		//al.stream().filter(i->i%2==0).collect(Collectors.toList());
 		//			.filter(line -> !"mkyong".equals(line))  
